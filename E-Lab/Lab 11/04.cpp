39ddb43d0e6e8f6489d689382ba58683b26d0d8c
iDiot-PC <b.aphisit2112@gmail.com>
2023-03-09 22:54:24 +0700

    09/03/2023|22:54

--- E-Lab/Lab 11/04.cpp
+++ E-Lab/Lab 11/04.cpp
@@ -29,22 +29,6 @@ public:
     void sing();
 };
 
-class Pet 
-{
-protected:
-    string name;
-public:
-    Pet(string);
-    string getName();
-    void setName(string);
-};
-
-class OwnedBird : public Bird, public Pet
-{
-public:
-    OwnedBird(string, int, int, int);
-};
-
 Animal :: Animal(int hap, int engy, int full) {
     happiness = hap; energy = engy; fullness = full;
     limitStat();
@@ -94,46 +78,7 @@ void Bird::sing() {
     limitStat();
 }
 
-Pet :: Pet(string tag) { name = tag; }
-string Pet :: getName() { return name; }
-void Pet :: setName(string newtag) { name = newtag;  }
-
-OwnedBird::OwnedBird(string name, int hap, int engy, int full) : Pet(name), Bird(hap, engy, full) {}
 
 int main()
 {
-    char action;
-    int hour;
-    int happiness, energy, fullness;
-    cin >> happiness >> energy >> fullness;
-    OwnedBird animal("Dobby", happiness, energy, fullness);
-    while (1) {
-        cin >> action;
-        if (action == 'q') break;
-        cin >> hour;
-        switch (action) {
-            case 'e':
-                animal.eat(hour);
-                break;
-            case 'p':
-                animal.play(hour);
-                break;
-            case 's':
-                animal.sleep(hour);
-                break;
-            case 'f':
-                animal.fly(hour);
-                break;
-            case 'i':
-                for (int i = 0; i < hour; i++)
-                    animal.sing();
-                break;
-            case 'n':
-                if (hour < 5)
-                    animal.setName("Debby");
-                else
-                    animal.setName("Shogun");
-                break;
-        }
-    }
 }
